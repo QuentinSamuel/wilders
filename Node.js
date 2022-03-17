2c294942d1a08546c617c75579adf94f5913a870
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-03-17 12:14:28 +0100

    Add name with color in terminal

---
+++ Node.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('Quentin !'));
+
+console.log(chalk.blue('Simon !'));
+
+console.log(chalk.green('Théo !'));
+
+console.log(chalk.yellow('Jaafar !'));
